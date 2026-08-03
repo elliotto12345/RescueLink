6b1aad0e07e65142cd658cc56ccf5e22ae96de12
zia260901 <kezia.osei.sarpong@gmail.com>
2026-08-03 01:58:12 +0000

    Added branding/styling assets

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = function (api) {
+  {
+    api.cache(true);
+    return {
+      presets: ["module:metro-react-native-babel-preset"],
+      plugins: [
+        [
+          "module:react-native-dotenv",
+          {
+            moduleName: "@env",
+            path: ".env",
+            blacklist: null,
+            whitelist: null,
+            safe: true,
+            allowUndefined: true,
+          },
+        ],
+      ],
+    };
+  }
+};
